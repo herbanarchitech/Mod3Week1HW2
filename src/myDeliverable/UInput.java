bd789361b9e55388b17053cd92b4ace3111a306e
herbanarhitech <jeff.badette@protonmail.com>
2021-10-05 02:48:43 -0400

    second commit

--- src/myDeliverable/UInput.java
+++ src/myDeliverable/UInput.java
@@ -9,57 +9,54 @@ public class UInput {
 		// TODO Auto-generated method stub
 
 
-		Scanner inputCheck = new Scanner(System.in);
+		Scanner userInput = new Scanner(System.in);
 		System.out.println("Please enter a number.");
 		
 		
-		int userInt = inputCheck.nextInt();
+		int uInt = userInput.nextInt(); // used to accept the integer input from the keyboard
+		
+//		 this will be used to enter the beginning and ending for the fibbinoacci sequence
+		
+		System.out.println("fibonacci Series");
+		int integerone= 0, integertwo = 1;
+		int n3 = uInt;
+		System.out.println(integerone);
+		System.out.println(integerone);
 		
 		
-//		1) Write Fibonanacci series between the user entered start and end values
 		
-		System.out.println("---Fibonacci Series---");
-		int n1 = 0, n2 = 1;
-		int n3 = userInt;
-		System.out.println(n1);
-		System.out.println(n2);
-		
-		
-		
-		for(int i=2;i < userInt; ++i) {
-			n3=n1+n2;
+		for(int i=2;i < uInt; ++i) {
+			n3=integerone+integertwo;
 			System.out.println("" + n3);
-			n1=n2;
-			n2=n3;
+			integerone=integertwo;
+			integertwo=n3;
 			continue;
 		}
 		
 		
 		
 		System.out.printf("Armstrong Check\n");
-		int originalNumber, remainder, result = 0, n = 0;
+		int originalINT, remainder, result = 0, n = 0;
 		
-		originalNumber = userInt;
+		originalINT = uInt;
 		
-		for(;originalNumber !=0; originalNumber/=10, ++n);
+		for(;originalINT !=0; originalINT/=10, ++n);
 				
-		originalNumber = userInt;
+		originalINT = uInt;
 		
-		for(;originalNumber !=0; originalNumber/=10) {
-			remainder = originalNumber % 10;
+		for(;originalINT !=0; originalINT/=10) {
+			remainder = originalINT % 10;
 			result += Math.pow(remainder, n);
 		}
-		
-		if(result == userInt)
-			System.out.printf( "%d is an Armstrong number.", userInt);
-		else
-			System.out.println(userInt + " is not an Armstrong number.");
-		
-//		3) Write a program to see if a user entered string is a palindrome or not
-		
-		String userInput = inputCheck.next();
-		
+		userInput.close();
 
+		if(result == uInt)
+			System.out.printf( "%d is an Armstrong number.", uInt);
+		else
+			System.out.printf(" %d is not an Armstrong number.", uInt);
+		
+//		
+		
 	}
 
 }
